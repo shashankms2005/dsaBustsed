46e1f926ce26950ef387224772e341d402a001d0
shashankms2005 <116255975+shashankms2005@users.noreply.github.com>
2022-10-20 13:34:46 +0530

    Add files via upload

---
+++ codnass.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    bool c;
+    cout<<"enter two num"<<endl;
+    cin>>a>>b;
+    if (a<50 && a<b)
+    {
+        /* code */
+     c=false;
+    }
+    else
+    c=true;
+    cout<<c<<endl;
+    return 0;
+}
