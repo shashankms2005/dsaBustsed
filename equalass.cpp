d6399c7b2ae0c4c563e7f17ce4899a71f8552c94
shashankms2005 <116255975+shashankms2005@users.noreply.github.com>
2022-10-20 12:52:23 +0530

    Add files via upload

---
+++ equalass.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter two num:"<<endl;
+    cin>>a>>b;
+    if(a==b)
+    
+    cout<<true<<endl;
+    else
+    cout<<false<<endl;
+    
+    return 0;
+}
